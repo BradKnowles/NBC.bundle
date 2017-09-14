c9914bb39edf6a190f99c71a16e2ddbcc7eecbb5
shopgirl284 <shopgirl284@tutamail.com>
2017-09-14 18:00:32 -0400

    NBC - Rewrite of channel plugin
    Rewrite of channel plugin using website api

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,12 +1,17 @@
 NAME = 'NBC'
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
+PREFIX = '/video/nbc'
 
-SHOWS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/nbce/containers/iPadRetina'
-SECTIONS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/nbce/asset/iPadRetina/%s'
-VIDEOS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/nbce/containers/iPadRetina/%s?seasonNumber=%s&filterBy=%s'
+BASE_URL = 'http://www.nbc.com'
+# active=1(shows) and published=1(videos) hide shows and videos that currently do not work, entitlement=free hides locked videos, and include=image includes the image URLs in json
+SHOWS_URL = 'https://api.nbc.com/v3.14/shows?filter[active]=1&include=image&page[size]=30&sort=sortTitle'
+VIDEO_URL = 'https://api.nbc.com/v3.14/videos?filter[entitlement]=free&filter[published]=1&include=image&page[size]=30&sort=-airdate'
 
-VIDEO_URL = 'http://www.nbc.com/%s/video/%s/%s#%s|%s' # show, title, short episode_id, show_id, long episode_id
+# API can be filtered by any field by adding &filter[filter name]=filter value (spaces must be %20)
+FILTER = '&filter[%s]=%s'
+
+GENRES = ['Drama', 'Comedy', 'Family and Kids', 'LateNight', 'Reality and Game Show', 'Reality', 'Talk and Interview', 'News and Information', 'Live Events and Specials']
 
 ####################################################################################################
 def Start():
@@ -16,148 +21,211 @@ def Start():
 	HTTP.Headers['User-Agent'] = 'BRNetworking/2.7.0.1449 (iPad;iPhone OS-8.1)'
 
 ####################################################################################################
-@handler('/video/nbc', NAME, art=ART, thumb=ICON)
-def Shows():
+@handler(PREFIX, NAME, art=ART, thumb=ICON)
+def MainMenu():
 
 	oc = ObjectContainer()
 
-	for show in JSON.ObjectFromURL(SHOWS_URL)['results']:
+	oc.add(DirectoryObject(key = Callback(FullEpSections, title="Episodes"), title = "Latest Full Episodes"))
+	oc.add(DirectoryObject(key = Callback(ShowSections, title="Shows"), title = "Shows"))
 
-		show_id = show['assetID']
-		title = show['title']
-		summary = show['description']
+	return oc
 
-		try:
-			thumb = show['images'][0]['images']['show_thumbnail_16_by_9']
-		except:
-			thumb = ''
+####################################################################################################
+@route(PREFIX + '/showsections')
+def ShowSections(title):
+
+	oc = ObjectContainer(title2=title)
+
+	oc.add(DirectoryObject(key = Callback(ShowList, title="Current Shows", url=SHOWS_URL + FILTER %('category', 'Current')), title = "Current Shows"))
+	oc.add(DirectoryObject(key = Callback(ShowList, title="Classic Shows", url=SHOWS_URL + FILTER %('category', 'Classic')), title = "Classic Shows"))
+	oc.add(DirectoryObject(key = Callback(Genres, title="Shows By Genre", url=SHOWS_URL), title = "Shows By Genre"))
+	oc.add(DirectoryObject(key = Callback(ShowList, title="All Shows", url=SHOWS_URL), title = "All Shows"))
+
+	return oc
+
+####################################################################################################
+@route(PREFIX + '/fullepsections')
+def FullEpSections(title, url=''):
+
+	oc = ObjectContainer(title2=title)
+
+	# Add full episode filter to url
+	fullep_url = VIDEO_URL + FILTER %('type', 'Full%20Episode')
+	for day_type in ['Primetime', 'LateNight', 'Daytime', ]:
+		day_url = fullep_url + FILTER %('dayPart', day_type)
+		oc.add(DirectoryObject(key = Callback(VideoList, title="%s Episodes" %day_type, url=day_url), title="%s Episodes" %day_type))
+	oc.add(DirectoryObject(key = Callback(Genres, title="Full Episodes By Genre", url=fullep_url), title = "Full Episodes By Genre"))
+	oc.add(DirectoryObject(key = Callback(VideoList, title="All Full Episodes", url=fullep_url), title = "All Full Episodes"))
+
+	return oc
+
+####################################################################################################
+# Function to produce shows or videos by genre
+@route(PREFIX + '/genres')
+def Genres(title, url):
+
+	oc = ObjectContainer(title2=title)
+
+	for item in GENRES:
+
+		sort_url = url + FILTER %('genre', String.Quote(item, usePlus = False))
+
+		if 'Shows' in title:
+			# Skip the genre fields that do not work for shows
+			if item in ['Family and Kids', 'Talk and Interview']:
+				continue
+			oc.add(DirectoryObject(
+				key = Callback(ShowList, title=item, url=sort_url),
+				title = item
+			))
+		else:
+			# Skip the genre fields that do not work for videos
+			if item in ['LateNight', 'Reality']:
+				continue
+			oc.add(DirectoryObject(
+				key = Callback(VideoList, title=item, url=sort_url),
+				title = item
+			))
+
+	return oc
+
+####################################################################################################
+@route(PREFIX + '/showlist')
+def ShowList(title, url):
+
+	oc = ObjectContainer(title2=title)
+
+	#show_attirbutes = '&fields[shows]=shortTitle,shortDescription'
+	json = JSON.ObjectFromURL(url)
+
+	for show in json['data']:
+
+		show_title = show['attributes']['shortTitle']
+		summary = show['attributes']['shortDescription']
+
+		thumb_id = show['relationships']['image']['data']['id']
+		thumb = GetImage(thumb_id, json)
+
+		show_id = show['id']
+		show_url = VIDEO_URL + FILTER %('show', show_id)
+		ag_id = show['relationships']['aggregates']['data']['id']
 
 		oc.add(DirectoryObject(
-			key = Callback(Sections, show_id=show_id, show=title),
-			title = title,
+			key = Callback(Aggregates, title=show_title, url=show_url, ag_id=ag_id, thumb=thumb),
+			title = show_title,
 			summary = summary,
 			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
 		))
 
-	oc.objects.sort(key=lambda obj: Regex('^The ').split(obj.title)[-1])
-	return oc
-
-####################################################################################################
-@route('/video/nbc/{show_id}/sections')
-def Sections(show_id, show):
-
-	oc = ObjectContainer(title2=show)
-	json_obj = JSON.ObjectFromURL(SECTIONS_URL % (show_id))
-
-	try:
-		thumb = json_obj['images'][0]['images']['show_thumbnail_16_by_9'] 
-	except:
-		thumb = ''
-
-	has_episodes = False
-	has_clips = False
-
-	for season in json_obj['seasons']:
-
-		if season['hasEpisodes']:
-			has_episodes = True
-
-		if season['hasClips']:
-			has_clips = True
-
-	if has_episodes:
-
-		oc.add(DirectoryObject(
-			key = Callback(Seasons, show_id=show_id, show=show, filter_by='episode'),
-			title = 'Full Episodes',
-			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+	try: next_page = json['links']['next']
+	except: next_page = None
+	if next_page:
+		oc.add(NextPageObject(
+			key = Callback(ShowList, title=title, url=next_page),
+			title = 'Next Page ...'
 		))
 
-	if has_clips:
+	if len(oc) < 1:
+		return ObjectContainer(header="Empty", message="There are no results to list.")
+	else:
+		return oc
+
+####################################################################################################
+# This function pulls the video types from the aggregate json for a show
+@route(PREFIX + '/aggregates')
+def Aggregates(title, url, ag_id, thumb=''):
+
+	oc = ObjectContainer(title2=title)
+
+	local_url = 'https://api.nbc.com/v3.14/aggregatesShowProperties/' + ag_id
+	try: json = JSON.ObjectFromURL(local_url)
+	except: return ObjectContainer(header="Invalid URL", message="The URL is invalid or does not contain json.")
+
+	vidtype_list = json['data']['attributes']['videoTypes']
+
+	for item in vidtype_list:
+
+		sort_url = url + FILTER %('type', String.Quote(item, usePlus = False))
 
 		oc.add(DirectoryObject(
-			key = Callback(Seasons, show_id=show_id, show=show, filter_by='clip'),
-			title = 'Clips',
+			key = Callback(VideoList, title=item, url=sort_url),
+			title = item,
 			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
 		))
 
 	return oc
 
 ####################################################################################################
-@route('/video/nbc/{show_id}/{filter_by}/seasons')
-def Seasons(show_id, show, filter_by):
+@route(PREFIX + '/videolist')
+def VideoList(title, url):
 
-	oc = ObjectContainer(title2=show)
-	json_obj = JSON.ObjectFromURL(SECTIONS_URL % (show_id))
+	oc = ObjectContainer(title2=title)
 
-	try:
-		thumb = json_obj['images'][0]['images']['show_thumbnail_16_by_9'] 
-	except:
-		thumb = ''
+	#video_attributes = '&fields[videos]=title,description,airdate,fullUrl,runTime,episodeNumber,seasonNumber,categories'
+	try: json = JSON.ObjectFromURL(url)
+	except: return ObjectContainer(header="Invalid URL", message="The URL is invalid or does not contain json.")
 
-	for season in json_obj['seasons']:
+	for episode in json['data']:
 
-		if filter_by == 'episode' and season['hasEpisodes']:
+		vid_url = episode['attributes']['fullUrl']
 
-			oc.add(DirectoryObject(
-				key = Callback(Videos, show_id=show_id, show=show, filter_by='episode', season=season['number']),
-				title = 'Season %s' % (season['number']),
-				thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
-			))
+		date = Datetime.ParseDate(episode['attributes']['airdate'])
 
-		elif filter_by == 'clip' and season['hasClips']:
+		duration = episode['attributes']['runTime']
+		duration = int(duration) * 1000
 
-			oc.add(DirectoryObject(
-				key = Callback(Videos, show_id=show_id, show=show, filter_by='clip', season=season['number']),
-				title = 'Season %s' % (season['number']),
-				thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
-			))
+		show_name = '' 
+		for item in episode['attributes']['categories']: 
+			if item.startswith('Series'):
+				show_name = item.split('Series/')[1]
+				break
 
-	return oc
-
-####################################################################################################
-@route('/video/nbc/{show_id}/{filter_by}/{season}/videos')
-def Videos(show_id, show, filter_by, season):
-
-	oc = ObjectContainer(title2=show)
-
-	for episode in JSON.ObjectFromURL(VIDEOS_URL % (show_id, season, filter_by))['results']:
-
-		if episode['requiresAuth']:
-			continue
-
-		episode_id = episode['assetID']
-
-		# Get show directory of url
-		parent_id = episode['parentContainerId']
-
-		title = episode['title']
-
-		# Remove parenthesis, apsotrophe, question mark, comma, period, dash, and colon from title for url build
-		url_title = title.replace("(", "").replace(")", "").replace("'", "").replace('?', '').replace(',', '').replace('-', '').replace('.', '').replace(":", "")
-
-		# Change title spaces and slashes to dashes and make lowercase for url build
-		url_title = url_title.replace('/', '-').replace(' ', '-').lower()
-
-		url = VIDEO_URL % (parent_id, url_title, episode_id.split('_')[-1], show_id, episode_id)
-		url = String.StripDiacritics(url)
-
-		try: ep_index = int(episode['episodeNumber'])
+		try: ep_index = int(episode['attributes']['episodeNumber'])
 		except: ep_index = None
+		try: ep_season = int(episode['attributes']['seasonNumber'])
+		except: ep_season = None
 
-		try: thumb = episode['images'][0]['images']['video_detail_16_by_9']
+		try: 
+			thumb_id = episode['relationships']['image']['data']['id']
+			thumb = GetImage(thumb_id, json)
 		except: thumb = ''
 
 		oc.add(EpisodeObject(
-			url = url,
-			show = show,
-			title = title,
-			summary = episode['description'],
+			url = vid_url,
+			show = show_name,
+			title = episode['attributes']['title'],
+			summary = episode['attributes']['description'],
 			thumb = Resource.ContentsOfURLWithFallback(url=thumb),
-			season = int(episode['seasonNumber']),
+			season = ep_season,
 			index = ep_index,
-			duration = episode['totalDuration'],
-			originally_available_at = Datetime.FromTimestamp(episode['firstAiredDate'])
+			duration = duration,
+			originally_available_at = date
 		))
 
-	return oc
+	try: next_page = json['links']['next']
+	except: next_page = None
+	if next_page:
+		oc.add(NextPageObject(
+			key = Callback(VideoList, title=title, url=next_page),
+			title = 'Next Page ...'
+		))
+
+	if len(oc) < 1:
+		return ObjectContainer(header="Empty", message="There are no results to list.")
+	else:
+		return oc
+####################################################################################################
+# This function pulls the image url from the included image data using the image id for each show or video
+@route(PREFIX + '/getimage')
+def GetImage(thumb_id, json):
+
+	image_url =''
+	for item in json['included']:
+		if item['id'] == thumb_id:
+			image_url = item['attributes']['path']
+			break
+
+	image = BASE_URL + image_url
+
+	return image
